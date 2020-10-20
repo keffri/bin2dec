876b58ebf6ea8d6667a14b41225225c340000f12
keffri <keffri.w.neal@gmail.com>
2020-10-19 17:39:44 -0700

    git init + styles

---
+++ script.js
@@ -0,0 +1,5 @@
+let zeroButton = document.querySelector(".zero");
+let oneButton = document.querySelector(".one");
+
+let binaryDisplay = document.querySelector(".binary");
+let decimalDisplay = document.querySelector(".decimal");
