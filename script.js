bf356b1f6a1805c92a2c25aaf720f4adbe73fcc6
keffri <keffri.w.neal@gmail.com>
2020-10-19 18:56:56 -0700

    functionality

--- script.js
+++ script.js
@@ -2,4 +2,51 @@ let zeroButton = document.querySelector(".zero");
 let oneButton = document.querySelector(".one");
 
 let binaryDisplay = document.querySelector(".binary");
+let binaryText = binaryDisplay.textContent;
 let decimalDisplay = document.querySelector(".decimal");
+let decimalText = decimalDisplay.textContent;
+let decimalNumber = 0;
+
+let clearButton = document.querySelector(".clear");
+let convertButton = document.querySelector(".convert");
+
+/*** Functions ***/
+
+reset = () => {
+  binaryText = "";
+};
+
+/*** Event Listeners ***/
+
+zeroButton.addEventListener("click", () => {
+  binaryText += zeroButton.textContent;
+  binaryDisplay.textContent = binaryText;
+  decimalDisplay.textContent = "";
+  decimalText = "";
+  decimalNumber = 0;
+});
+
+oneButton.addEventListener("click", () => {
+  binaryText += oneButton.textContent;
+  binaryDisplay.textContent = binaryText;
+  decimalDisplay.textContent = "";
+  decimalText = "";
+  decimalNumber = 0;
+});
+
+convertButton.addEventListener("click", () => {
+  for (let i = binaryText.length - 1; i >= 0; i--) {
+    decimalNumber +=
+      parseInt(binaryText[i]) * Math.pow(2, binaryText.length - 1 - i);
+  }
+  decimalDisplay.textContent = decimalNumber;
+  reset();
+  return decimalNumber;
+});
+
+clearButton.addEventListener("click", () => {
+  binaryDisplay.textContent = "";
+  binaryText = "";
+  decimalDisplay.textContent = "";
+  decimalText = "";
+});
